e98f2942b68f5fa0450fad40a864bb55bf267315
Kit Gallagher <gallagherkit@outlook.com>
2022-02-21 11:42:13 +0000

    Fix pixel size of saved images

--- birdspy/image_analysis.py
+++ birdspy/image_analysis.py
@@ -17,14 +17,19 @@ class BirdImage:
         self.file_loc = file_loc
         self.image = Image.open(file_loc)
         self.data = np.array(self.image)
+        self.pixel_size = (self.data.shape[1], self.data.shape[0])
 
-    def print(self, save_loc = ""):
+    def print(self, save_loc = "", my_dpi = 96):
         """Plots image object, with optional argument to save image"""
-        plt.imshow(self.data, interpolation='nearest')
-        axs = plt.gca()
+        fig = plt.figure(figsize=(self.pixel_size[0] / my_dpi, 
+                                  self.pixel_size[1] / my_dpi),
+                         dpi=my_dpi)
+        axs = fig.add_subplot()
+
+        axs.imshow(self.data, interpolation='nearest')
         axs.axes.xaxis.set_visible(False), axs.yaxis.set_visible(False)
         plt.axis('off')
-        if save_loc != "":
-            plt.savefig(save_loc)
-        plt.show()
 
+        if save_loc != "":
+            plt.savefig(save_loc, dpi=my_dpi)
+        plt.show()
