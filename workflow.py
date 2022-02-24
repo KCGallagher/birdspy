97e98f9b261d5605bfbc3fdc11940dd89ed65b7c
Kit Gallagher <gallagherkit@outlook.com>
2022-02-24 13:14:19 +0000

    Change handling of empty files

--- workflow.py
+++ workflow.py
@@ -14,10 +14,11 @@ import birdspy as bs
 # Model 2
 
 # Generate training and testing datasets
-bs.DatasetFactory.generate_datasets(
-    train_path=os.path.join("Counting_MATLAB_package_birds", "bird_data", "train"),
-    test_path=os.path.join("Counting_MATLAB_package_birds", "bird_data", "test"),
-    image_path=os.path.join("images", "HVITa_renamed", "HVITa2016a_renamed"),
-    test_frac=8,
-)
+for sub_folder in ["HVITa2016a_renamed", "HVITa2016b_renamed"]:
+    bs.DatasetFactory.generate_datasets(
+        train_path=os.path.join("Counting_MATLAB_package_birds", "bird_data", "train"),
+        test_path=os.path.join("Counting_MATLAB_package_birds", "bird_data", "test"),
+        image_path=os.path.join("images", "HVITa_renamed", sub_folder),
+        test_frac=8,
+    )
 
